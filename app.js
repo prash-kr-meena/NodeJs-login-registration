ad4b7d34bb1137c8cec13629121250cb2d4bc385
prashant-kr-314 <prashantkr314@gmail.com>
2018-08-30 21:32:16 +0530

    basic routing * setting up views * connecting to DB - tested

--- app.js
+++ app.js
@@ -1,31 +1,117 @@
 // jshint node : true
 "use strict";
 
-const express = require("express"),
-      path = require("path");
+const EXPRESS = require("express"),
+      MONGOOSE = require("mongoose"), // middleware to connect node app & mongoDB | Elegant MongoDB object modeling for Node.js
+      BODY_PARSER = require("body-parser"),
+      PATH = require("path");
 
-const app = express();
-const port = 3000;
-
-// load view-engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const APP = EXPRESS();
+const PORT = 3000;
 
 
-app.get('/', (req, res) => {
-      res.render("index", {
-            title: "Hello"
+
+//! body parser config
+// support parsing of application/json type post data
+APP.use(BODY_PARSER.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+APP.use(BODY_PARSER.urlencoded({
+      extended: true
+}));
+
+
+
+
+
+APP.use(EXPRESS.static("public"));
+
+//! load view-engine
+APP.set('views', PATH.join(__dirname, 'views'));
+APP.set('view engine', 'ejs');
+
+
+
+
+// ! Bring MODLES ;)
+const Article = require("./modles/article");
+
+
+
+
+// ! connect to mongoDB && Some checks
+// Mongoose provides a straight-forward, schema-based solution to model your application data.
+//  It includes built-in type casting, validation, query building, business logic hooks and more, out of the box.
+MONGOOSE.connect('mongodb://localhost/Authentication_App_DB', {
+      useNewUrlParser: true
+});
+const DB = MONGOOSE.connection;
+
+
+//? check connection
+DB.once('open', () => {
+      console.log('Connected to DB :  SUCCESS');
+});
+
+// ? check db errors
+DB.on('error', (db_err) => {
+      console.log("DB ERROR : " + db_err);
+});
+
+
+
+
+// ? ---------------------------   ROUTES      ---------------------------
+
+APP.get('/', (req, res) => {
+
+      Article.find({}, (err, all_articles) => {
+            if (err) {
+                  throw new Error("ERROR : finding article in DB");
+            }
+
+            let renderVar = {
+                  render_page: "./pages/index",
+                  page_title: "Home Page",
+                  all_articles: all_articles
+            };
+
+            res.render("template", renderVar);
       });
 });
 
 
-app.get('/articles/add', (req, res) => {
-      res.render('add_article', {
-            title : 'Add article'
-      });
+
+
+APP.get('/articles/add', (req, res) => {
+
+      let renderVar = {
+            render_page: "./pages/add_article", // index.ejs
+            page_title: "Add Articles"
+      };
+      res.render("template", renderVar);
+
 });
 
 
-app.listen(port, function () {
-      console.log("server live  : " + port);
+// ? -------------------------------------------------------------------------
+
+
+
+// ! listen
+APP.listen(PORT, () => {
+      console.log(`Remember :  start mongoDB demon -->   mongod   &&   mongo `);
+      console.log(`-- Server live : ${PORT} --`);
 });
+
+
+
+
+
+
+// * fake data for inserting db
+// db.articles.insert({
+//       title: "article_7",
+//       author: "malini seth",
+//       body: "Lorem ipsum reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+// });
